eb853ff2d6974993ce8eb6d2c0c396cdae3fffd9
Overkane <overkanex@gmail.com>
2026-05-02 21:09:28 +0500

    Health component refactor and new function

--- lib/components/general/health/component_health.gd
+++ lib/components/general/health/component_health.gd
@@ -10,13 +10,13 @@ signal health_depleted()
 		max_health = value
 		max_health_changed.emit(value)
 
-var current_health := 0.0:
+var current_health := 1.0:
 	set(value):
-		print("New health" + str(value) + " " + str(get_parent()))
+		print("%s new health is %s." % [owner.name, value])
 		current_health = clampf(value, 0, max_health)
 		current_health_changed.emit(current_health)
 		if is_zero_approx(current_health):
-			print("%s health depleted" % get_parent())
+			print("%s health depleted." % owner.name)
 			health_depleted.emit()
 
 
@@ -27,6 +27,10 @@ func _ready() -> void:
 
 func get_current_health_percentage() -> float:
 	return current_health / max_health
+
+func set_health(health: float) -> void:
+	max_health = health
+	current_health = health
 
 func take_damage(damage: float) -> void:
 	current_health -= damage
